3ef99d8c316a12e41e2589c06204bb0bd6376fdc
CKachur <CKachur1105@gmail.com>
2016-01-29 19:59:58 -0500

    Starting the best game ever...duh

---
+++ desktop/src/com/ckachur/glarbs/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ckachur.glarbs.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ckachur.glarbs.Glarbs;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Glarbs(), config);
+	}
+}
